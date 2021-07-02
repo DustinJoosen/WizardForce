7cd87db80490c2e82cdb774f6ec8eeadc6d348ff
DustinJoosen <dustinjoosen2003@gmail.com>
2021-07-02 11:42:44 +0200

    Play the music

--- code/WizardForce/app.py
+++ code/WizardForce/app.py
@@ -2,9 +2,13 @@ import pygame
 from widgets.Button import Button
 import events.ButtonEvents
 from Enums import EventType
+from data.models.Settings import Settings
 from ScreenManager import ScreenManager
 
 pygame.init()
+pygame.mixer.init()
+
+BACKGROUND_MUSIC_FP = "../WizardForce/resources/music/background_music.mp3"
 
 WIDTH = 800
 HEIGHT = 530
@@ -16,6 +20,14 @@ clock = pygame.time.Clock()
 FPS = 24
 
 screen_manager = ScreenManager(screen)
+
+#music settings
+settings = Settings.Load()
+if settings.play_music:
+	pygame.mixer.music.load(BACKGROUND_MUSIC_FP)
+	pygame.mixer.music.play(-1)
+
+	pygame.mixer.music.set_volume(0.01 * settings.music_volume)
 
 while screen_manager.running:
 	clock.tick(FPS)
--- code/WizardForce/data/models/Settings.py
+++ code/WizardForce/data/models/Settings.py
@@ -2,6 +2,11 @@ from data.providers.JsonProvider import JsonProvider
 
 
 class Settings(JsonProvider):
+	def __init__(self):
+		self.play_music = None
+		self.music_volume = 0
+		self.flash_red_on_damage = None
+
 	@classmethod
 	def Load(cls):
 		settings = Settings()
